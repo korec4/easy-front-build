6f973f899c6bff87a57762216041ceded68e837a
alejandro.caravantes@obnn.com <L0l1t4#$>
2018-04-20 11:36:54 -0500

    test(index.test.js): added test functions

--- test/index.test.js
+++ test/index.test.js
@@ -2,19 +2,53 @@
 const core = require('../lib/index');
 const fs = require('fs');
 const sinon = require('sinon');
+const dummy = require('./sinonObjects/writeDirectory.dummy');
+
 
 test('writeDirectory', (done) => {
-
     const stub = sinon.stub(fs, 'mkdirSync').callsFake(() => {
-        return undefined;
+        return true;
     });
 
     const promise = core.WriteDirectory('front_modules', 'petter');
 
     expect.assertions(1);
     return promise.then(data => {
-        expect(data).toBe(false);
+        expect(data).toBe(true);
         done();
+        stub.restore();
     });
 });
 
+test('WriteFile', () => {
+
+    const stub = sinon.stub(fs, 'writeFileSync').callsFake(() => {
+       return true;
+    });
+
+    const promise = core.WriteFile(dummy.templateHTML,'text/html', 'html');
+
+    expect.assertions(1);
+    return promise.then(data => {
+        expect(data).toBe(true);
+        stub.restore();
+    });
+});
+
+test('ReadFile', (done) => {
+    const stub = sinon.stub(fs, 'readFile').callsFake(() => {
+        return dummy.templateHTML;
+    });
+
+    const promise = core.ReadFile('templateHTML', 'testFile');
+
+    expect.assertions(0);
+
+    return promise.then(data => {
+        expect(data).toBe(dummy.templateHTML);
+        done();
+        stub.restore();
+    })
+
+
+});
--- test/sinonObjects/writeDirectory.dummy.js
+++ test/sinonObjects/writeDirectory.dummy.js
@@ -1,3 +1,7 @@
 'use strict';
 
-const sinon = require('sinon');
+const templateHTML = `<!DOCTYPE html>\n<html lang = "en" >\n<head>\n<meta charset="utf-8">\n<title>Example of Fetch</title>\n</head>\n<body>\n<h1></h1></body><script type="text/javascript" src="${name}.js"></script></html>`;
+const templateCSS = `${name}{\n}`;
+const templateJS = `(function ${name} () {\n\t}();`;
+
+module.exports = {templateHTML, templateCSS, templateJS};
